a86e6d55a054165141c009e0453ff95861212bbf
Facundo Garcia <facundogg7@gmail.com>
2021-06-10 19:10:20 -0300

    incorporacion de activo en el nav

--- routes/contacto.js
+++ routes/contacto.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contacto');
+  res.render('contacto', {
+    isContacto:true
+  });
 });
 
 module.exports = router;
--- routes/proyecto.js
+++ routes/proyecto.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('proyecto');
+  res.render('proyecto', {
+    isProyecto:true
+  });
 });
 
 module.exports = router;
