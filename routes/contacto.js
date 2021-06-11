2f39a0896629283a4c303e33b7ae8cc6ac7c117d
Facundo Garcia <facundogg7@gmail.com>
2021-06-10 22:48:04 -0300

    formulario de contacto andando

--- routes/contacto.js
+++ routes/contacto.js
@@ -3,10 +3,38 @@ var router = express.Router();
 var nodemailer = require('nodemailer');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('contacto', {
-    isContacto:true
+    isContacto: true
   });
 });
 
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var telefono = req.body.telefono;
+  var email = req.body.email;
+  var comentarios = req.body.comentarios;
+
+  //console.log(req.body);
+
+  var obj = {
+    to: 'madeinchacoargentina@gmail.com',
+    subjet: 'Contacto desde la web',
+    html: nombre + ' ' + apellido + ' se contacto a traves de la web y este es su correo: ' + email + '<br> Hizo el siguiente comentario: ' + comentarios + '<br> Su numero de contacto es: ' + telefono
+  }
+
+  var transport = nodemailer.createTransport({
+    host:process.env.SMTP_HOST,
+    port:process.env.SMTP_PORT,
+    auth:{
+      user:process.env.SMTP_USER,
+      pass:process.env.SMTP_PASS
+    }
+  });
+
+  var info = await transport.sendMail(obj);
+  res.render('contacto', {message: 'mensaje enviado correctamente!'});
+});
 module.exports = router;
