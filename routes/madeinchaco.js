8e187910b9422601ffc639a665ca087f57b5cbcf
Facundo Garcia <facundogg7@gmail.com>
2021-06-14 13:16:27 -0300

    nuevo layout para index y madeinchaco

--- routes/madeinchaco.js
+++ routes/madeinchaco.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('madeinchaco');
+  res.render('madeinchaco', {
+    layout:'layout2'
+  });
 });
 
 module.exports = router;
