802276fc465822dcf988695f54be8fd3401a7824
Facundo Garcia <facundogg7@gmail.com>
2021-07-13 11:51:05 -0300

    prueba heroku cloudinary

--- routes/admin/contenido.js
+++ routes/admin/contenido.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var router = express.Router();
 var contenidoModel = require("../../models/contenidoModel");
-var cloudinary = require("cloudinary");
+var cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
 
-  cloud_name: process.env.CLOUDINARY_NAME,
+  /* cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  cloudinary_url: process.env.CLOUDINARY_URL 
+  cloudinary_url: process.env.CLOUDINARY_URL  */
   
 });
 
@@ -51,7 +51,7 @@ router.post("/agregar", async (req, res, next) => {
       req.body.subtitulo != "" &&
       req.body.cuerpo != ""
     ) {
-      var result = await cloudinary.v2.uploader.upload(req.file.path);
+      var result = await cloudinary.uploader.upload(req.file.path);
       console.log(result);
 
       await contenidoModel.insertContenido(req.body, result.url);
@@ -110,7 +110,7 @@ router.post("/modificar", async (req, res, next) => {
 
     if (req.file.path != "") {
 
-      var result = await cloudinary.v2.uploader.upload(req.file.path);
+      var result = await cloudinary.uploader.upload(req.file.path);
       await contenidoModel.modificarContenidoByIDImg(obj, result.url, req.body.id);
       await fs.unlink(req.file.path);
       res.redirect("/admin/contenido");
